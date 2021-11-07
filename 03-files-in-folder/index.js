d89c34a95300dba68f526cf331dddad7a17f8084
Ilya Dzyuin <ilua.dzuin@gmail.com>
2021-11-07 12:32:23 +0400

    feat: add copy-directory solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,13 +4,24 @@ const path = require('path');
 const dirPath = path.join(__dirname, 'secret-folder');
 
 fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
-	files.forEach(file => {
-		if (file.isFile()) {
-			fs.stat(path.join(dirPath, file.name), (err, stats) => {
-				console.log(file.name.substring(0, file.name.lastIndexOf('.')).padEnd(10), ' - ',
-					file.name.split('.').pop().padEnd(4), ' - ',
-					(stats.size + ' B').padStart(10));
-			});
-		}
-	})
+	if (err) {
+		throw err
+	} else {
+		files.forEach(file => {
+			if (file.isFile()) {
+				fs.stat(path.join(dirPath, file.name), (err, stats) => {
+					if (err) {
+						throw err
+					} else {
+						fileName = file.name.substring(0, file.name.lastIndexOf('.'));
+						fileExt = file.name.split('.').pop();
+						fileSize = stats.size + ' B';
+						console.log(fileName.padEnd(10),
+							' - ', fileExt.padEnd(4),
+							' - ', fileSize.padStart(10));
+					}
+				});
+			}
+		})
+	}
 });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+const copyDir = async (srcDir, newDir) => {
+	try {
+		await fs.rm(newDir, { force: true, recursive: true, })
+		await fs.mkdir(newDir);
+		const files = await fs.readdir(srcDir, { withFileTypes: true });
+		files.forEach(file => {
+			if (file.isDirectory()) {
+				copyDir(path.join(srcDir, file.name),
+					path.join(newDir, file.name))
+			} else {
+				fs.copyFile(path.join(srcDir, file.name),
+					path.join(newDir, file.name));
+			}
+		})
+		console.log(`${srcDir}\ncopied to\n${newDir}`)
+	} catch (err) { throw err }
+}
+
+copyDir(path.join(__dirname, 'files'),
+	path.join(__dirname, 'files-copy'));
